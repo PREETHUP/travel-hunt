c051c66c894a3e69c0ffe8d570ea8a6cc24ab31d
A-7327 <A-7327>
2018-02-25 11:09:09 +0530

    First Working Version after intergration

---
+++ src/main/java/com/ndchack/travelhunt/ui/domain/Ancilaries/UpdateAncillary.java
@@ -0,0 +1,25 @@
+package com.ndchack.travelhunt.ui.domain.Ancilaries;
+
+/**
+ * Created by a-6281 on 2/25/18.
+ */
+public class UpdateAncillary {
+    String tasksFinished;
+    String totalTasks;
+
+    public String getTasksFinished() {
+        return tasksFinished;
+    }
+
+    public void setTasksFinished(String tasksFinished) {
+        this.tasksFinished = tasksFinished;
+    }
+
+    public String getTotalTasks() {
+        return totalTasks;
+    }
+
+    public void setTotalTasks(String totalTasks) {
+        this.totalTasks = totalTasks;
+    }
+}
